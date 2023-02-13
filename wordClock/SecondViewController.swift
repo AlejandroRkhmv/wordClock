1f06f62d78423226cfb95a328cb30ad807f39251
Alexander-Rakhimov <alexanderrkhmv@gmail.com>
2023-02-13 21:03:07 +0400

    change icons

--- wordClock/SecondViewController.swift
+++ wordClock/SecondViewController.swift
@@ -12,27 +12,27 @@ class SecondViewController: UIViewController {
     let model = Model.model
     let mainAnimator = UIDynamicAnimator()
     var aboutLabels = [UILabel]()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         createAboutLabels()
         addGesture()
     }
-    
+
     //MARK: create about labels
-    
+
     private func createAboutLabels() {
-        
+
         let sideOfLabel = self.view.bounds.size.width / 12
         var count = 0
         var positionX = self.view.center.x - (sideOfLabel * 4.5)
         var positionY = self.view.center.y - (sideOfLabel * 4)
-        
+
         for letter in model.aboutString.uppercased() {
-            
+
             count += 1
-            
+
             let newLabel = UILabel(frame: CGRect(x: 0, y: 0, width: sideOfLabel, height: sideOfLabel))
             newLabel.center = CGPoint(x: positionX, y: positionY)
             newLabel.text = String(letter)
@@ -42,7 +42,7 @@ class SecondViewController: UIViewController {
             newLabel.backgroundColor = self.view.backgroundColor
             aboutLabels.append(newLabel)
             self.view.addSubview(newLabel)
-            
+
             positionX += sideOfLabel
             if count == 10 {
                 positionY += sideOfLabel * 2
@@ -59,45 +59,41 @@ class SecondViewController: UIViewController {
             }
         }
     }
-    
+
     //MARK: add gesture
     private func addGesture() {
-        
+
         let gesture = UITapGestureRecognizer(target: self, action: #selector(createAnimation))
         self.view.addGestureRecognizer(gesture)
     }
-    
-    
-    
+
+
+
     //MARK: create animation
-    
+
     @objc func createAnimation() {
-        
+
         model.tapCountForAboutScreen += 1
-        
+
         let labelBehavior = UIDynamicItemBehavior(items: aboutLabels)
         for label in aboutLabels {
             let randomFloat = Float.random(in: 1...3)
             labelBehavior.addAngularVelocity(CGFloat(randomFloat), for: label)
         }
         mainAnimator.addBehavior(labelBehavior)
-        
+
         if model.tapCountForAboutScreen == 2 {
-            
+
             let gravity = UIGravityBehavior(items: aboutLabels)
             gravity.magnitude = 0.5
             mainAnimator.addBehavior(gravity)
-            
+
             //MARK: POP
             DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2), execute: { [self] in
-                
+
                 self.navigationController?.popViewController(animated: false)
                 model.tapCountForAboutScreen = 0
             })
         }
-        
     }
-
-  
-
 }
