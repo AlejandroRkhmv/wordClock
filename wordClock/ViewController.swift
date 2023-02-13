2de5f75c1d093bd7668956def5d16a179c45ae99
Alexander-Rakhimov <alexanderrkhmv@gmail.com>
2023-02-13 14:27:07 +0400

    create center position container view when landscape orientation

--- wordClock/ViewController.swift
+++ wordClock/ViewController.swift
@@ -7,9 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     let model = Model.model
+    
+    var initialOrientation = true
+    var portraitOrientation: Bool {
+        get {
+            if view.frame.width < view.frame.height {
+                initialOrientation = true
+                return true
+            } else {
+                initialOrientation = false
+                return false
+            }
+        }
+    }
     
     let settingButton = UIButton()
     let containerView = UIView()
@@ -25,23 +38,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createContainerView()
-        createWordLabels()
-        createNoMarkMinutes()
+        start()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        createSettingButton()
-        createContainerView()
         DispatchQueue.main.async {
             self.myTimer = Timer.scheduledTimer(timeInterval: TimeInterval(self.period), target: self, selector: #selector(self.forTimer), userInfo: nil, repeats: true)
         }
         createGestureToMainView()
         settingButton.addTarget(self, action: #selector(pushSettingButton), for: .touchUpInside)
-       
+        settingButton.setTitleColor(model.textColor, for: .normal)
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        
+        if !portraitOrientation {
+            settingButton.isHidden = true
+        } else {
+            settingButton.isHidden = false
+        }
+        
+        settingButton.isHidden.toggle()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        containerView.center = self.view.center
+    }
+    
+    private func start() {
+        createSettingButton()
+        createContainerView()
+        createWordLabels()
+        createNoMarkMinutes()
+    }
+    
     
     //MARK: add gesture to mainView
     private func createGestureToMainView() {
@@ -52,13 +84,14 @@ class ViewController: UIViewController {
     
     //MARK: - create views and word labels
     private func createContainerView() {
-        
-        let sideOfContainerView = (self.view.bounds.size.width / 12) * 11
-        
+        var sideOfContainerView: CGFloat = 0
+        if initialOrientation {
+            sideOfContainerView = (view.bounds.size.width / 12) * 11
+        } else {
+            sideOfContainerView = (view.bounds.size.height / 12) * 11
+        }
         containerView.frame = CGRect(x: 0, y: 0, width: sideOfContainerView, height: sideOfContainerView)
-        containerView.center = self.view.center
         self.view.addSubview(containerView)
-        
     }
     
     private func createWordLabels() {
@@ -131,15 +164,20 @@ class ViewController: UIViewController {
     //MARK: create setting button
     
     private func createSettingButton() {
-        
-        let rightPadding = self.view.bounds.size.width / 17
-        
-        settingButton.frame = CGRect(x: 0, y: 0, width: 100, height: 30)
-        settingButton.center = CGPoint(x: Int(self.view.bounds.size.width) - Int(rightPadding) - 50, y: 100)
+        guard portraitOrientation else { return }
+        settingButton.translatesAutoresizingMaskIntoConstraints = false
         settingButton.setTitle("ADJUST", for: .normal)
         settingButton.setTitleColor(model.textColor, for: .normal)
         settingButton.titleLabel?.font = UIFont.init(name: "Courier", size: 25)
         self.view.addSubview(settingButton)
+        setConstraintsForSettingButton()
+    }
+    
+    internal func setConstraintsForSettingButton() {
+        settingButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        settingButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        settingButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: (self.view.bounds.size.width / 17) - 50).isActive = true
+        settingButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: (self.view.bounds.size.width / 17) + 50).isActive = true
     }
     
     //MARK: add @objc func
@@ -156,7 +194,7 @@ class ViewController: UIViewController {
     }
     
     @objc func tapForGoToNextScreen(gesture: UITapGestureRecognizer) {
-        
+        guard portraitOrientation else { return }
         myTimer.invalidate()
         let secondVC = SecondViewController()
         self.navigationController?.pushViewController(secondVC, animated: false)
@@ -176,7 +214,7 @@ class ViewController: UIViewController {
     }
 
     @objc func pushSettingButton() {
-
+        guard portraitOrientation else { return }
         myTimer.invalidate()
         let settingVC = SettingViewController()
         self.navigationController?.pushViewController(settingVC, animated: false)
